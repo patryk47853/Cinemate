3d553a5952ace2ce74dc050093fa303f59adac3f
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-03-01 19:30:55 +0100

    Initial commit

---
+++ src/test/java/pl/patrykjava/cinemate/CinemateApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patrykjava.cinemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
