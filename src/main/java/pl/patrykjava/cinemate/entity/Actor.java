0c93b58432bd7ff30e2c42ce8623f8a869c5cae6
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-03-02 11:40:52 +0100

    Create Actor, Category, Director and Movie entities

---
+++ src/main/java/pl/patrykjava/cinemate/entity/Actor.java
@@ -0,0 +1,20 @@
+package pl.patrykjava.cinemate.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
---
+++ src/main/java/pl/patrykjava/cinemate/entity/Movie.java
@@ -0,0 +1,29 @@
+package pl.patrykjava.cinemate.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private Double rating;
+    private String description;
+    private String imgUrl;
+    @ManyToMany
+    private List<Category> categories;
+    @ManyToOne
+    private Director director;
+    @ManyToMany
+    private List<Actor> actors;
+}
