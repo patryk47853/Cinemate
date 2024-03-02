a240d3d6e231b84a350662f145b55969ff80c5ad
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-03-02 11:33:11 +0100

    Create User entity

---
+++ src/main/java/pl/patrykjava/cinemate/entity/User.java
@@ -0,0 +1,24 @@
+package pl.patrykjava.cinemate.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String imgUrl;
+}
