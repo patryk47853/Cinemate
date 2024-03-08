209e5c8f5e4f266437c8c5838b924f7e404b2d8a
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-03-08 08:42:17 +0100

    Add MemberRowMapper for JDBC Access Service

---
+++ src/main/java/pl/patrykjava/cinemate/member/MemberRowMapper.java
@@ -0,0 +1,19 @@
+package pl.patrykjava.cinemate.member;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class MemberRowMapper implements RowMapper<Member> {
+    @Override
+    public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Member(
+                rs.getLong("id"),
+                rs.getString("username"),
+                rs.getString("email"),
+                rs.getString("password"));
+    }
+}
