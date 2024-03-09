60acf9300c738e2bfdbc008ce26e26ee24ddbad5
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-03-09 19:22:16 +0100

    Set up all tests for MemberJDBCDataAccessService (all passed)

--- src/test/java/pl/patrykjava/cinemate/member/MemberJDBCDataAccessServiceTest.java
+++ src/test/java/pl/patrykjava/cinemate/member/MemberJDBCDataAccessServiceTest.java
@@ -1,20 +1,21 @@
 package pl.patrykjava.cinemate.member;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.core.JdbcTemplate;
+import pl.patrykjava.cinemate.AbstractTestcontainers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
 
-class MemberJDBCDataAccessServiceTest {
+class MemberJDBCDataAccessServiceTest extends AbstractTestcontainers {
 
     private MemberJDBCDataAccessService memberJDBCDataAccessService;
-    private MemberRowMapper memberRowMapper = new MemberRowMapper();
+    private final MemberRowMapper memberRowMapper = new MemberRowMapper();
 
     @BeforeEach
     void setUp() {
         memberJDBCDataAccessService = new MemberJDBCDataAccessService(
-                new JdbcTemplate(),
+                getJdbcTemplate(),
                 memberRowMapper
         );
     }
@@ -22,72 +23,372 @@ class MemberJDBCDataAccessServiceTest {
     @Test
     void selectAllMembers() {
         //Given
+        Member member = new Member(
+                FAKER.name().username() + UUID.randomUUID(),
+                FAKER.internet().safeEmailAddress() + UUID.randomUUID(),
+                "test123"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
 
         //When
+        var actual = memberJDBCDataAccessService.selectAllMembers();
 
         //Then
+        Assertions.assertThat(actual).isNotEmpty();
     }
 
     @Test
     void selectMemberById() {
         //Given
+        String email = FAKER.internet().safeEmailAddress() + UUID.randomUUID();
+
+        Member member = new Member(
+                FAKER.name().username() + UUID.randomUUID(),
+                email,
+                "test123"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
+
+        Long id = memberJDBCDataAccessService.selectAllMembers()
+                .stream()
+                .filter(m -> m.getEmail().equals(email))
+                .map(Member::getId)
+                .findFirst()
+                .orElseThrow();
 
         //When
+        var actual = memberJDBCDataAccessService.selectMemberById(id);
 
         //Then
+        Assertions.assertThat(actual).isPresent().hasValueSatisfying(m -> {
+            Assertions.assertThat(m.getId()).isEqualTo(id);
+            Assertions.assertThat(m.getUsername()).isEqualTo(member.getUsername());
+            Assertions.assertThat(m.getEmail()).isEqualTo(member.getEmail());
+        });
+    }
+
+    @Test
+    void willReturnEmptyWhenSelectCustomerById() {
+        //Given
+        Long id = -1L;
+
+        //When
+        var actual = memberJDBCDataAccessService.selectMemberById(id);
+
+        //Then
+        Assertions.assertThat(actual).isEmpty();
     }
 
     @Test
     void insertMember() {
         //Given
+        String username = FAKER.name().username() + UUID.randomUUID();
+
+        Member member = new Member(
+                username,
+                FAKER.internet().safeEmailAddress() + UUID.randomUUID(),
+                "password"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
 
         //When
+        boolean actual = memberJDBCDataAccessService.selectAllMembers()
+                .stream()
+                .anyMatch(m -> m.getUsername().equals(username));
 
         //Then
+        Assertions.assertThat(actual).isTrue();
     }
 
     @Test
     void existsMemberWithEmail() {
         //Given
+        String email = FAKER.internet().safeEmailAddress() + UUID.randomUUID();
+
+        Member member = new Member(
+                FAKER.name().username() + UUID.randomUUID(),
+                email,
+                "password"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
 
         //When
+        boolean actual = memberJDBCDataAccessService.existsMemberWithEmail(email);
+
 
         //Then
+        Assertions.assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsPersonWithEmailReturnsFalseWhenDoesNotExists() {
+        //Given
+        String email = FAKER.internet().safeEmailAddress() + UUID.randomUUID();
+
+        //When
+        boolean actual = memberJDBCDataAccessService.existsMemberWithEmail(email);
+
+        //Then
+        Assertions.assertThat(actual).isFalse();
     }
 
     @Test
     void existsMemberWithId() {
         //Given
+        String username = FAKER.name().username() + UUID.randomUUID();
+
+        Member member = new Member(
+                username,
+                FAKER.internet().safeEmailAddress() + UUID.randomUUID(),
+                "password"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
+
+        Long id = memberJDBCDataAccessService.selectAllMembers()
+                .stream()
+                .filter(m -> m.getUsername().equals(username))
+                .map(Member::getId)
+                .findFirst()
+                .orElseThrow();
 
         //When
+        boolean actual = memberJDBCDataAccessService.existsMemberWithId(id);
 
         //Then
+        Assertions.assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsMemberWithIdReturnsFalseWhenDoesNotExists() {
+        //Given
+        Long id = -1L;
+
+        //When
+        boolean actual = memberJDBCDataAccessService.existsMemberWithId(id);
+
+        //Then
+        Assertions.assertThat(actual).isFalse();
     }
 
     @Test
     void existsMemberWithUsername() {
         //Given
+        String username = FAKER.name().username() + UUID.randomUUID();
+
+        Member member = new Member(
+                username,
+                FAKER.internet().safeEmailAddress() + UUID.randomUUID(),
+                "password"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
 
         //When
+        boolean actual = memberJDBCDataAccessService.existsMemberWithUsername(username);
 
         //Then
+        Assertions.assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsMemberWithUsernameReturnsFalseWhenDoesNotExists() {
+        //Given
+        String username = FAKER.name().username() + UUID.randomUUID();
+
+        //When
+        boolean actual = memberJDBCDataAccessService.existsMemberWithUsername(username);
+
+        //Then
+        Assertions.assertThat(actual).isFalse();
     }
 
     @Test
     void deleteMemberById() {
         //Given
+        String username = FAKER.name().username() + UUID.randomUUID();
+
+        Member member = new Member(
+                username,
+                FAKER.internet().safeEmailAddress() + UUID.randomUUID(),
+                "password"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
+
+        int sizeAfterInsertion = memberJDBCDataAccessService.selectAllMembers().size();
+
+        Long id = memberJDBCDataAccessService.selectAllMembers()
+                .stream()
+                .filter(m -> m.getUsername().equals(username))
+                .map(Member::getId)
+                .findFirst()
+                .orElseThrow();
 
         //When
+        memberJDBCDataAccessService.deleteMemberById(id);
+
+        int actual = memberJDBCDataAccessService.selectAllMembers().size();
 
         //Then
+        Assertions.assertThat(actual).isEqualTo(sizeAfterInsertion - 1);
     }
 
     @Test
-    void updateMember() {
+    void updateMemberUsername() {
         //Given
+        String username = FAKER.name().username() + UUID.randomUUID();
+
+        Member member = new Member(
+                username,
+                FAKER.internet().safeEmailAddress() + UUID.randomUUID(),
+                "password"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
+
+        Long id = memberJDBCDataAccessService.selectAllMembers()
+                .stream()
+                .filter(m -> m.getUsername().equals(username))
+                .map(Member::getId)
+                .findFirst()
+                .orElseThrow();
+
+        String newUsername = "Patryk";
 
         //When
+        Member update = new Member();
+        update.setId(id);
+        update.setUsername(newUsername);
+
+        memberJDBCDataAccessService.updateMember(update);
+        //Then
+        var actual = memberJDBCDataAccessService.selectMemberById(id);
+
+        Assertions.assertThat(actual).isPresent().hasValueSatisfying(m -> {
+            Assertions.assertThat(m.getId()).isEqualTo(id);
+            Assertions.assertThat(m.getUsername()).isEqualTo(newUsername);
+            Assertions.assertThat(m.getEmail()).isEqualTo(member.getEmail());
+        });
+    }
+
+    @Test
+    void updateMemberEmail() {
+        //Given
+        String email = FAKER.internet().safeEmailAddress() + UUID.randomUUID();
+
+        Member member = new Member(
+                FAKER.name().username() + UUID.randomUUID(),
+                email,
+                "password"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
+
+        Long id = memberJDBCDataAccessService.selectAllMembers()
+                .stream()
+                .filter(m -> m.getEmail().equals(email))
+                .map(Member::getId)
+                .findFirst()
+                .orElseThrow();
+
+        String newEmail = "patryk@gmail.com";
+
+        //When
+        Member update = new Member();
+        update.setId(id);
+        update.setEmail(newEmail);
+
+        memberJDBCDataAccessService.updateMember(update);
+        //Then
+        var actual = memberJDBCDataAccessService.selectMemberById(id);
+
+        Assertions.assertThat(actual).isPresent().hasValueSatisfying(m -> {
+            Assertions.assertThat(m.getId()).isEqualTo(id);
+            Assertions.assertThat(m.getUsername()).isEqualTo(member.getUsername());
+            Assertions.assertThat(m.getEmail()).isEqualTo(newEmail);
+        });
+    }
+
+    @Test
+    void willUpdateAllPropertiesMember() {
+        //Given
+        String username = FAKER.name().username() + UUID.randomUUID();
+
+        Member member = new Member(
+                username,
+                FAKER.internet().safeEmailAddress() + UUID.randomUUID(),
+                "password"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
+
+        Long id = memberJDBCDataAccessService.selectAllMembers()
+                .stream()
+                .filter(m -> m.getUsername().equals(username))
+                .map(Member::getId)
+                .findFirst()
+                .orElseThrow();
+
+        String newUsername = "John";
+        String newEmail = "john@gmail.com";
+
+        //When
+        Member update = new Member();
+        update.setId(id);
+        update.setUsername(newUsername);
+        update.setEmail(newEmail);
+
+        memberJDBCDataAccessService.updateMember(update);
+        //Then
+        var actual = memberJDBCDataAccessService.selectMemberById(id);
+
+        Assertions.assertThat(actual).isPresent().hasValueSatisfying(m -> {
+                    Assertions.assertThat(m.getId()).isEqualTo(id);
+                    Assertions.assertThat(m.getUsername()).isEqualTo(newUsername);
+                    Assertions.assertThat(m.getEmail()).isEqualTo(newEmail);
+                }
+        );
+    }
+
+    @Test
+    void willNotUpdateWhenNothingToUpdate() {
+        //Given
+        String username = FAKER.name().username() + UUID.randomUUID();
+
+        Member member = new Member(
+                username,
+                FAKER.internet().safeEmailAddress() + UUID.randomUUID(),
+                "password"
+        );
+
+        memberJDBCDataAccessService.insertMember(member);
+
+        Long id = memberJDBCDataAccessService.selectAllMembers()
+                .stream()
+                .filter(m -> m.getUsername().equals(username))
+                .map(Member::getId)
+                .findFirst()
+                .orElseThrow();
+
+        //When
+        Member update = new Member();
+        update.setId(id);
+
+        memberJDBCDataAccessService.updateMember(update);
 
         //Then
+        var actual = memberJDBCDataAccessService.selectMemberById(id);
+
+        Assertions.assertThat(actual).isPresent().hasValueSatisfying(m -> {
+                    Assertions.assertThat(m.getId()).isEqualTo(id);
+                    Assertions.assertThat(m.getUsername()).isEqualTo(member.getUsername());
+                    Assertions.assertThat(m.getEmail()).isEqualTo(member.getEmail());
+                }
+        );
     }
 }
