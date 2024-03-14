55672f2429ccb21380029291ea852196233f36dd
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-03-14 09:39:30 +0100

    Add abstract methods in CategoryDao

--- src/main/java/pl/patrykjava/cinemate/category/CategoryDao.java
+++ src/main/java/pl/patrykjava/cinemate/category/CategoryDao.java
@@ -1,4 +1,16 @@
 package pl.patrykjava.cinemate.category;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryDao {
+    Optional<Category> selectCategoryById(Long id);
+    Optional<Category> selectCategoryByName(String name);
+    List<Category> selectAllCategories();
+    boolean existsCategoryWithName(String name);
+    boolean existsCategoryWithId(Long id);
+    void insertCategory(Category category);
+    void deleteCategoryById(Long id);
+    void deleteCategoryByName(String name);
+    void updateCategory(Category category);
 }
