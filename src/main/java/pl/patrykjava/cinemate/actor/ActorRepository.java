78fce9f2a4a188149c613721e2feb25543217372
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-03-19 08:41:16 +0100

    Add new methods to Actor and Director Repository

--- src/main/java/pl/patrykjava/cinemate/actor/ActorRepository.java
+++ src/main/java/pl/patrykjava/cinemate/actor/ActorRepository.java
@@ -2,6 +2,14 @@ package pl.patrykjava.cinemate.actor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ActorRepository extends JpaRepository<Actor, Long> {
+    boolean existsActorByLastName(String lastName);
+    boolean existsActorByFirstNameAndLastName(String firstName, String lastName);
+    Optional<List<Actor>> findActorsByLastName(String lastName);
+    boolean existsActorById(Long id);
 }
--- src/main/java/pl/patrykjava/cinemate/director/DirectorRepository.java
+++ src/main/java/pl/patrykjava/cinemate/director/DirectorRepository.java
@@ -3,6 +3,13 @@ package pl.patrykjava.cinemate.director;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DirectorRepository extends JpaRepository<Director, Long> {
+    boolean existsDirectorByLastName(String lastName);
+    boolean existsDirectorByFirstNameAndLastName(String firstName, String lastName);
+    Optional<List<Director>> findDirectorsByLastName(String lastName);
+    boolean existsDirectorById(Long id);
 }
