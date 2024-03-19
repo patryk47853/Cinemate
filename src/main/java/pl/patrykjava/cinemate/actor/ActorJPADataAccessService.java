05b302f348e1da95aff3fefe50854484d7221ab1
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-03-19 08:39:30 +0100

    Implement methods of Director and Actor DAO

--- src/main/java/pl/patrykjava/cinemate/actor/ActorJPADataAccessService.java
+++ src/main/java/pl/patrykjava/cinemate/actor/ActorJPADataAccessService.java
@@ -1,4 +1,62 @@
 package pl.patrykjava.cinemate.actor;
 
-public class ActorJPADataAccessService {
+import org.springframework.stereotype.Repository;
+import pl.patrykjava.cinemate.actor.Actor;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ActorJPADataAccessService implements ActorDao {
+    
+    private final ActorRepository actorRepository;
+
+    public ActorJPADataAccessService(ActorRepository actorRepository) {
+        this.actorRepository = actorRepository;
+    }
+
+    @Override
+    public Optional<Actor> selectActorById(Long id) {
+        return actorRepository.findById(id);
+    }
+
+    @Override
+    public Optional<List<Actor>> selectActorsByLastName(String lastName) {
+        return actorRepository.findActorsByLastName(lastName);
+    }
+
+    @Override
+    public boolean existsActorWithLastName(String lastName) {
+        return actorRepository.existsActorByLastName(lastName);
+    }
+
+    @Override
+    public boolean existsActorWithFullName(String firstName, String lastName) {
+        return actorRepository.existsActorByFirstNameAndLastName(firstName, lastName);
+    }
+
+    @Override
+    public boolean existsActorWithId(Long id) {
+        return actorRepository.existsActorById(id);
+    }
+
+    @Override
+    public List<Actor> selectAllActors() {
+        return actorRepository.findAll();
+    }
+
+    @Override
+    public void insertActor(Actor actor) {
+        actorRepository.save(actor);
+    }
+
+    @Override
+    public void deleteActorById(Long id) {
+        actorRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateActor(Actor actor) {
+        actorRepository.save(actor);
+    }
 }
--- src/main/java/pl/patrykjava/cinemate/director/DirectorJPADataAccessService.java
+++ src/main/java/pl/patrykjava/cinemate/director/DirectorJPADataAccessService.java
@@ -1,4 +1,60 @@
 package pl.patrykjava.cinemate.director;
 
-public class DirectorJPADataAccessService {
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class DirectorJPADataAccessService implements DirectorDao {
+    private final DirectorRepository directorRepository;
+
+    public DirectorJPADataAccessService(DirectorRepository directorRepository) {
+        this.directorRepository = directorRepository;
+    }
+
+    @Override
+    public Optional<Director> selectDirectorById(Long id) {
+        return directorRepository.findById(id);
+    }
+
+    @Override
+    public Optional<List<Director>> selectDirectorsByLastName(String lastName) {
+        return directorRepository.findDirectorsByLastName(lastName);
+    }
+
+    @Override
+    public boolean existsDirectorWithLastName(String lastName) {
+        return directorRepository.existsDirectorByLastName(lastName);
+    }
+
+    @Override
+    public boolean existsDirectorWithFullName(String firstName, String lastName) {
+        return directorRepository.existsDirectorByFirstNameAndLastName(firstName, lastName);
+    }
+
+    @Override
+    public boolean existsDirectorWithId(Long id) {
+        return directorRepository.existsDirectorById(id);
+    }
+
+    @Override
+    public List<Director> selectAllDirectors() {
+        return directorRepository.findAll();
+    }
+
+    @Override
+    public void insertDirector(Director director) {
+        directorRepository.save(director);
+    }
+
+    @Override
+    public void deleteDirectorById(Long id) {
+        directorRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateDirector(Director director) {
+        directorRepository.save(director);
+    }
 }
