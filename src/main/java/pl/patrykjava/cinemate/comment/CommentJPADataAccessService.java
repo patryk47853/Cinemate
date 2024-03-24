87419c221bbb33007f397bc3e1c2ba277cde072e
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-03-24 09:38:40 +0100

    Implement Comment and Movie JPA Data Access Service

--- src/main/java/pl/patrykjava/cinemate/comment/CommentJPADataAccessService.java
+++ src/main/java/pl/patrykjava/cinemate/comment/CommentJPADataAccessService.java
@@ -1,4 +1,76 @@
 package pl.patrykjava.cinemate.comment;
 
-public class CommentJPADataAccessService {
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CommentJPADataAccessService implements CommentDao {
+
+    private final CommentRepository commentRepository;
+
+    public CommentJPADataAccessService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public Optional<Comment> selectCommentById(Long id) {
+        return commentRepository.findById(id);
+    }
+
+    @Override
+    public Optional<List<Comment>> selectCommentsByMemberId(Long id) {
+        return commentRepository.findAllByMemberId(id);
+    }
+
+    @Override
+    public Optional<List<Comment>> selectCommentsByMovieId(Long id) {
+        return commentRepository.findAllByMovieId(id);
+    }
+
+    @Override
+    public List<Comment> selectAllComments() {
+        return commentRepository.findAll();
+    }
+
+    @Override
+    public boolean existsCommentWithMemberId(Long id) {
+        return commentRepository.existsCommentByMemberId(id);
+    }
+
+    @Override
+    public boolean existsCommentWithMovieId(Long id) {
+        return commentRepository.existsCommentByMovieId(id);
+    }
+
+    @Override
+    public boolean existsCommentWithId(Long id) {
+        return commentRepository.existsCommentById(id);
+    }
+
+    @Override
+    public void insertComment(Comment comment) {
+        commentRepository.save(comment);
+    }
+
+    @Override
+    public void deleteCommentById(Long id) {
+        commentRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteCommentsByMemberId(Long id) {
+        commentRepository.deleteAllByMemberId(id);
+    }
+
+    @Override
+    public void deleteCommentsByMovieId(Long id) {
+        commentRepository.deleteAllByMovieId(id);
+    }
+
+    @Override
+    public void updateComment(Comment comment) {
+        commentRepository.save(comment);
+    }
 }
--- src/main/java/pl/patrykjava/cinemate/movie/MovieJPADataAccessService.java
+++ src/main/java/pl/patrykjava/cinemate/movie/MovieJPADataAccessService.java
@@ -1,4 +1,81 @@
 package pl.patrykjava.cinemate.movie;
 
-public class MovieJPADataAccessService {
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MovieJPADataAccessService implements MovieDao {
+
+    private final MovieRepository movieRepository;
+
+    public MovieJPADataAccessService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public Optional<Movie> selectMovieById(Long id) {
+        return movieRepository.findById(id);
+    }
+
+    @Override
+    public Optional<List<Movie>> selectAllMoviesByCategoryId(Long id) {
+        return movieRepository.findAllByCategoriesId(id);
+    }
+
+    @Override
+    public Optional<List<Movie>> selectAllMoviesByActorId(Long id) {
+        return movieRepository.findAllByActorsId(id);
+    }
+
+    @Override
+    public Optional<List<Movie>> selectAllMoviesByDirectorId(Long id) {
+        return movieRepository.findAllByDirectorId(id);
+    }
+
+    @Override
+    public List<Movie> selectAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    @Override
+    public boolean existsMovieWithId(Long id) {
+        return movieRepository.existsById(id);
+    }
+
+    @Override
+    public boolean existsMovieWithCategoryId(Long id) {
+        return movieRepository.existsMovieByCategoriesContainingId(id);
+    }
+
+    @Override
+    public boolean existsMovieWithActorId(Long id) {
+        return movieRepository.existsMovieByActorsContainingId(id);
+    }
+
+    @Override
+    public boolean existsMovieWithDirectorId(Long id) {
+        return movieRepository.existsMovieByDirectorId(id);
+    }
+
+    @Override
+    public boolean existsMovieWithTitleAndDirectorId(String title, Long id) {
+        return movieRepository.existsMovieByTitleAndDirectorId(title, id);
+    }
+
+    @Override
+    public void insertMovie(Movie movie) {
+        movieRepository.save(movie);
+    }
+
+    @Override
+    public void deleteMovieById(Long id) {
+        movieRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateMovie(Movie movie) {
+        movieRepository.save(movie);
+    }
 }
