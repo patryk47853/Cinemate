7641e63282a64e6035d4e879861a9b3a55ad337e
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-04-26 09:14:28 +0200

    Implement main view

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {ChakraProvider} from "@chakra-ui/react";
+import {createStandaloneToast} from '@chakra-ui/react'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css'
+import Login from "./components/login/Login.jsx";
+import AuthProvider from "./components/context/AuthContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const {ToastContainer} = createStandaloneToast();
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <Login/>
+    },
+    {
+        path:"home",
+        element: <App />
+    }
+])
+
+ReactDOM
+    .createRoot(document.getElementById('root'))
+    .render(
+        <React.StrictMode>
+            <ChakraProvider>
+                <AuthProvider>
+                    <RouterProvider router={router} />
+                </AuthProvider>
+                <ToastContainer/>
+            </ChakraProvider>
+        </React.StrictMode>,
+    )
