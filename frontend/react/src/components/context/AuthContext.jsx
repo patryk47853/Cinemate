a59fbdadd595a1692adf5773eba2b3048b726229
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-04-26 09:14:42 +0200

    Provide AuthContext

---
+++ frontend/react/src/components/context/AuthContext.jsx
@@ -0,0 +1,42 @@
+import {
+    createContext,
+    useContext,
+    useEffect,
+    useState
+} from "react";
+import {login as performLogin} from "../../services/client.js";
+
+const AuthContext = createContext({});
+
+const AuthProvider = ({ children }) => {
+    const [member, setMember] = useState(null);
+
+    const login = async (usernameAndPassword) => {
+        return new Promise((resolve, reject) => {
+            performLogin(usernameAndPassword).then(res => {
+                const jwtToken = res.headers["authorization"];
+                localStorage.setItem("access_token", jwtToken)
+                console.log(jwtToken)
+                setMember({
+                    ...res.data.memberDto
+                })
+                resolve(res);
+            }).catch(err => {
+                reject(err);
+            })
+        })
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            member,
+            login
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => useContext(AuthContext);
+
+export default AuthProvider;
