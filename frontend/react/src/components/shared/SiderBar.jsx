100c6c3da747ed198026b98b9db241a916c105b4
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-05-02 10:26:44 +0200

    Cosmetic changes

--- frontend/react/src/components/shared/SiderBar.jsx
+++ frontend/react/src/components/shared/SiderBar.jsx
@@ -180,7 +180,7 @@ const MobileNav = ({onOpen, ...rest}) => {
                                 <Avatar
                                     size={'sm'}
                                     src={
-                                        member.imgUrl
+                                        'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
                                     }
                                 />
                                 <VStack
@@ -188,7 +188,7 @@ const MobileNav = ({onOpen, ...rest}) => {
                                     alignItems="flex-start"
                                     spacing="1px"
                                     ml="2">
-                                    <Text fontSize="sm">{member.username}</Text>
+                                    <Text fontSize="sm">{member?.username}</Text>
                                 </VStack>
                                 <Box display={{base: 'none', md: 'flex'}}>
                                     <FiChevronDown/>
