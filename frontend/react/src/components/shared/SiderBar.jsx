eea72548aea8735480e30f6da60727f920afea22
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-05-17 07:48:20 +0200

    Fetch member's profile

--- frontend/react/src/components/shared/SiderBar.jsx
+++ frontend/react/src/components/shared/SiderBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import {
     Avatar,
@@ -32,7 +32,9 @@ import {
     FiStar,
     FiTrendingUp, FiUsers
 } from 'react-icons/fi';
-import {useAuth} from "../context/AuthContext.jsx";
+import {getUserProfile, useAuth} from "../context/AuthContext.jsx";
+import {getMemberProfile, getMembers} from "../../services/client.js";
+import {errorNotification} from "../../services/notification.js";
 
 const LinkItems = [
     { name: 'Home', icon: FiHome, to: '/home' },
@@ -81,11 +83,12 @@ const SidebarContent = ({onClose, ...rest}) => {
             pos="fixed"
             h="full"
             {...rest}>
-            <Flex h="20" alignItems="center" mx="4" justifyContent="space-between">
+            <Flex h="90" alignItems="center" mx="4" justifyContent="space-between">
                 <Image
-                    mt={5}
-                    boxSize='100px'
-                    src='https://raw.githubusercontent.com/patryk47853/Cinemate/master/screenshots/logo.png'
+                    mt={1}
+                    boxSize='90px'
+                    src='https://raw.githubusercontent.com/patryk47853/Cinemate/master/screenshots/logo4.png'
+                    maxH={"70px"}
                     mx='auto'
                 />
                 <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
@@ -132,64 +135,97 @@ const NavItem = ({icon, children, ...rest}) => {
     );
 };
 
-const MobileNav = ({onOpen, ...rest}) => {
+const MobileNav = ({ onOpen, ...rest }) => {
     const { logout, member } = useAuth();
+    const [userProfile, setUserProfile] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        if (member) {
+            fetchMemberProfile(member.username);
+        }
+    }, [member]);
+
+    const fetchMemberProfile = (username = member.username) => {
+        setLoading(true);
+        getMemberProfile(username)
+            .then(res => {
+                setUserProfile(res.data);
+            })
+            .catch(err => {
+                if (err.response && err.response.data) {
+                    setError(err.response.data.message);
+                    errorNotification(
+                        err.code,
+                        err.response.data.message
+                    );
+                } else {
+                    setError("An error occurred while fetching user profile.");
+                    errorNotification("Error", "An error occurred while fetching user profile.");
+                }
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+
     return (
         <Flex
-            ml={{base: 0, md: 60}}
-            px={{base: 4, md: 4}}
+            ml={{ base: 0, md: 60 }}
+            px={{ base: 4, md: 4 }}
             height="20"
             alignItems="center"
             bg={useColorModeValue('white', 'gray.900')}
             borderBottomWidth="1px"
             borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-            justifyContent={{base: 'space-between', md: 'flex-end'}}
+            justifyContent={{ base: 'space-between', md: 'flex-end' }}
             {...rest}>
             <IconButton
-                display={{base: 'flex', md: 'none'}}
+                display={{ base: 'flex', md: 'none' }}
                 onClick={onOpen}
                 variant="outline"
                 aria-label="open menu"
-                icon={<FiMenu/>}
+                icon={<FiMenu />}
             />
 
             <Text
-                display={{base: 'flex', md: 'none'}}
+                display={{ base: 'flex', md: 'none' }}
                 fontSize="2xl"
                 fontFamily="monospace"
                 fontWeight="bold">
                 Logo
             </Text>
 
-            <HStack spacing={{base: '0', md: '6'}}>
+            <HStack spacing={{ base: '0', md: '6' }}>
                 <IconButton
                     size="lg"
                     variant="ghost"
                     aria-label="open menu"
-                    icon={<FiBell/>}
+                    icon={<FiBell />}
                 />
                 <Flex alignItems={'center'}>
                     <Menu>
                         <MenuButton
                             py={2}
                             transition="all 0.3s"
-                            _focus={{boxShadow: 'none'}}>
+                            _focus={{ boxShadow: 'none' }}>
                             <HStack>
-                                <Avatar
-                                    size={'sm'}
-                                    src={
-                                        'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                                    }
-                                />
+                                {userProfile && userProfile.imgUrl && (
+                                    <Avatar
+                                        size={'sm'}
+                                        src={userProfile.imgUrl}
+                                    />
+                                )}
                                 <VStack
-                                    display={{base: 'none', md: 'flex'}}
+                                    display={{ base: 'none', md: 'flex' }}
                                     alignItems="flex-start"
                                     spacing="1px"
                                     ml="2">
                                     <Text fontSize="sm">{member?.username}</Text>
                                 </VStack>
-                                <Box display={{base: 'none', md: 'flex'}}>
-                                    <FiChevronDown/>
+                                <Box display={{ base: 'none', md: 'flex' }}>
+                                    <FiChevronDown />
                                 </Box>
                             </HStack>
                         </MenuButton>
@@ -197,7 +233,7 @@ const MobileNav = ({onOpen, ...rest}) => {
                             bg={useColorModeValue('white', 'gray.900')}
                             borderColor={useColorModeValue('gray.200', 'gray.700')}>
                             <MenuItem>Profile</MenuItem>
-                            <MenuDivider/>
+                            <MenuDivider />
                             <MenuItem onClick={logout}>
                                 Sign out
                             </MenuItem>
