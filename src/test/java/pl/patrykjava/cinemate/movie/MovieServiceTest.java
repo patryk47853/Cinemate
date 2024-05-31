ae1e4e2df062a8b241ccd5fdf5c07c0cdb8edd41
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-05-31 11:12:04 +0200

    Fix tests

--- src/test/java/pl/patrykjava/cinemate/movie/MovieServiceTest.java
+++ src/test/java/pl/patrykjava/cinemate/movie/MovieServiceTest.java
@@ -1,13 +1,17 @@
 package pl.patrykjava.cinemate.movie;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.patrykjava.cinemate.actor.Actor;
+import pl.patrykjava.cinemate.actor.ActorService;
+import pl.patrykjava.cinemate.category.Category;
+import pl.patrykjava.cinemate.category.CategoryService;
 import pl.patrykjava.cinemate.director.Director;
+import pl.patrykjava.cinemate.director.DirectorService;
 import pl.patrykjava.cinemate.exception.DuplicateResourceException;
 import pl.patrykjava.cinemate.exception.ResourceNotFoundException;
 
@@ -17,50 +21,50 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MovieServiceTest {
 
-    private MovieService movieService;
-
     @Mock
     private MovieDao movieDao;
 
-    @BeforeEach
-    void setUp() {
-        movieService = new MovieService(movieDao);
-    }
+    @Mock
+    private ActorService actorService;
+
+    @Mock
+    private CategoryService categoryService;
+
+    @Mock
+    private DirectorService directorService;
+
+    @InjectMocks
+    private MovieService movieService;
 
     @Test
     void canGetMovie() {
-        //Given
+        // Given
         Long id = 1L;
-
-        Movie movie = new Movie(
-                1L,"Inception", 8.5, "A great movie", "inception.jpg",
-                new ArrayList<>(), new Director(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>()
-        );
+        Movie movie = new Movie(1L, "Inception", 8.5, "A great movie", "inception.jpg", "Oscar",
+                new ArrayList<>(), new Director(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
 
         when(movieDao.selectMovieById(id)).thenReturn(Optional.of(movie));
 
-        //When
+        // When
         Movie actual = movieService.getMovie(id);
 
-        //Then
+        // Then
         assertThat(actual).isEqualTo(movie);
     }
 
     @Test
     void willThrowWhenGetMovieReturnsEmptyOptional() {
-        //Given
+        // Given
         Long id = 1L;
 
         when(movieDao.selectMovieById(id)).thenReturn(Optional.empty());
 
-        //Then
+        // Then
         assertThatThrownBy(() -> movieService.getMovie(id))
                 .isInstanceOf(ResourceNotFoundException.class)
                 .hasMessage("No movie with ID: " + id + " has been found.");
@@ -68,10 +72,10 @@ class MovieServiceTest {
 
     @Test
     void getAllMovies() {
-        //When
+        // When
         movieService.getAllMovies();
 
-        //Then
+        // Then
         verify(movieDao).selectAllMovies();
     }
 
@@ -88,20 +92,19 @@ class MovieServiceTest {
         List<Movie> result = movieService.getAllMoviesByCategoryId(categoryId);
 
         // Then
-        Assertions.assertNotNull(result);
-        Assertions.assertEquals(2, result.size());
+        assertThat(result).isNotNull();
+        assertThat(result.size()).isEqualTo(2);
         verify(movieDao).selectAllMoviesByCategoryId(categoryId);
     }
 
-
     @Test
     void willThrowWhenNoMoviesWithCategoryId() {
-        //Given
+        // Given
         Long categoryId = 1L;
 
         when(movieDao.existsMovieWithCategoryId(categoryId)).thenReturn(false);
 
-        //Then
+        // Then
         assertThatThrownBy(() -> movieService.getAllMoviesByCategoryId(categoryId))
                 .isInstanceOf(ResourceNotFoundException.class)
                 .hasMessage("No movie with category ID: " + categoryId + " has been found.");
@@ -120,19 +123,19 @@ class MovieServiceTest {
         List<Movie> result = movieService.getAllMoviesByActorId(actorId);
 
         // Then
-        Assertions.assertNotNull(result);
-        Assertions.assertEquals(2, result.size());
+        assertThat(result).isNotNull();
+        assertThat(result.size()).isEqualTo(2);
         verify(movieDao).selectAllMoviesByActorId(actorId);
     }
 
     @Test
     void willThrowWhenNoMoviesWithActorId() {
-        //Given
+        // Given
         Long actorId = 1L;
 
         when(movieDao.existsMovieWithActorId(actorId)).thenReturn(false);
 
-        //Then
+        // Then
         assertThatThrownBy(() -> movieService.getAllMoviesByActorId(actorId))
                 .isInstanceOf(ResourceNotFoundException.class)
                 .hasMessage("No movie with actor ID: " + actorId + " has been found.");
@@ -151,19 +154,19 @@ class MovieServiceTest {
         List<Movie> result = movieService.getAllMoviesByDirectorId(directorId);
 
         // Then
-        Assertions.assertNotNull(result);
-        Assertions.assertEquals(2, result.size());
+        assertThat(result).isNotNull();
+        assertThat(result.size()).isEqualTo(2);
         verify(movieDao).selectAllMoviesByDirectorId(directorId);
     }
 
     @Test
     void willThrowWhenNoMoviesWithDirectorId() {
-        //Given
+        // Given
         Long directorId = 1L;
 
         when(movieDao.existsMovieWithDirectorId(directorId)).thenReturn(false);
 
-        //Then
+        // Then
         assertThatThrownBy(() -> movieService.getAllMoviesByDirectorId(directorId))
                 .isInstanceOf(ResourceNotFoundException.class)
                 .hasMessage("No movie with director ID: " + directorId + " has been found.");
@@ -171,84 +174,105 @@ class MovieServiceTest {
 
     @Test
     void addMovie() {
-        //Given
+        // Given
+        String categories = "Action, Sci-Fi";
+        String directorFullName = "Christopher Nolan";
+        String actors = "Leonardo DiCaprio, Joseph Gordon-Levitt";
+
         String title = "Inception";
-        Long directorId = 1L;
+        Double rating = 8.5;
+        String description = "A great movie";
+        String imgUrl = "inception.jpg";
+        String awards = "Oscar";
 
-        Director director = new Director(directorId, "Christopher", "Nolan", new ArrayList<>());
+        // Mocked director and actors
+        Director director = new Director(1L, "Christopher", "Nolan", new ArrayList<>());
+        Actor actor1 = new Actor(1L, "Leonardo", "DiCaprio", new ArrayList<>());
+        Actor actor2 = new Actor(2L, "Joseph", "Gordon-Levitt", new ArrayList<>());
 
-        when(movieDao.existsMovieWithTitleAndDirectorId(title, directorId)).thenReturn(false);
+        List<Category> parsedCategories = Arrays.asList(new Category(1L, "Action", new ArrayList<>()), new Category(2L, "Sci-Fi", new ArrayList<>()));
+
+        when(directorService.findOrCreateDirector("Christopher", "Nolan")).thenReturn(director);
+        when(actorService.findOrCreateActor("Leonardo", "DiCaprio")).thenReturn(actor1);
+        when(actorService.findOrCreateActor("Joseph", "Gordon-Levitt")).thenReturn(actor2);
+        when(categoryService.findOrCreateCategory("Action")).thenReturn(parsedCategories.get(0));
+        when(categoryService.findOrCreateCategory("Sci-Fi")).thenReturn(parsedCategories.get(1));
+        when(movieDao.existsMovieWithTitleAndDirectorId(title, director.getId())).thenReturn(false);
 
         MovieAddRequest request = new MovieAddRequest(
-                title, 8.5, "A great movie", "inception.jpg",
-                new ArrayList<>(), director, new ArrayList<>()
+                title, rating, description, imgUrl, awards, categories, directorFullName, actors
         );
-        //When
+
+        // When
         movieService.addMovie(request);
 
-        //Then
+        // Then
         ArgumentCaptor<Movie> movieArgumentCaptor = ArgumentCaptor.forClass(Movie.class);
-
         verify(movieDao).insertMovie(movieArgumentCaptor.capture());
-
         Movie actual = movieArgumentCaptor.getValue();
 
         assertThat(actual.getId()).isNull();
         assertThat(actual.getTitle()).isEqualTo(request.title());
+        assertThat(actual.getDirector()).isEqualTo(director);
+        assertThat(actual.getActors()).containsExactlyInAnyOrder(actor1, actor2);
+        assertThat(actual.getCategories()).containsExactlyInAnyOrderElementsOf(parsedCategories);
+        assertThat(actual.getAwards()).isEqualTo(request.awards());
+        assertThat(actual.getRating()).isEqualTo(request.rating());
+        assertThat(actual.getDescription()).isEqualTo(request.description());
+        assertThat(actual.getImgUrl()).isEqualTo(request.imgUrl());
     }
 
     @Test
     void willThrowWhenAddMovieWithExistingTitleAndDirectorId() {
-        //Given
+        // Given
         String title = "Inception";
-        Long directorId = 1L;
+        String directorFullName = "Christopher Nolan";
 
-        Director director = new Director(directorId, "Christopher", "Nolan", new ArrayList<>());
+        Director director = new Director(1L, "Christopher", "Nolan", new ArrayList<>());
 
-        when(movieDao.existsMovieWithTitleAndDirectorId(title, directorId)).thenReturn(true);
+        when(directorService.findOrCreateDirector("Christopher", "Nolan")).thenReturn(director);
+        when(movieDao.existsMovieWithTitleAndDirectorId(title, director.getId())).thenReturn(true);
 
         MovieAddRequest request = new MovieAddRequest(
-                title, 8.5, "A great movie", "inception.jpg",
-                new ArrayList<>(), director, new ArrayList<>()
+                title, 8.5, "A great movie", "inception.jpg", "Oscar", "Action, Sci-Fi", directorFullName, "Leonardo DiCaprio, Joseph Gordon-Levitt"
         );
 
-        //When
+        // When
         assertThatThrownBy(() -> movieService.addMovie(request))
                 .isInstanceOf(DuplicateResourceException.class)
                 .hasMessage("A movie with the same title and director already exists.");
 
-        //Then
+        // Then
         verify(movieDao, never()).insertMovie(any());
     }
 
     @Test
     void deleteMovieById() {
-        //Given
+        // Given
         Long id = 1L;
 
         when(movieDao.existsMovieWithId(id)).thenReturn(true);
 
-        //When
+        // When
         movieService.deleteMovieById(id);
 
-        //Then
+        // Then
         verify(movieDao).deleteMovieById(id);
     }
 
     @Test
     void willThrowWhenDeleteMovieByIdDoesNotExists() {
-        //Given
+        // Given
         Long id = 1L;
 
         when(movieDao.existsMovieWithId(id)).thenReturn(false);
 
-        //When
+        // When
         assertThatThrownBy(() -> movieService.deleteMovieById(id))
                 .isInstanceOf(ResourceNotFoundException.class)
                 .hasMessage("Movie with ID: " + id + " not found.");
 
-        //Then
+        // Then
         verify(movieDao, never()).deleteMovieById(any());
     }
 }
-
