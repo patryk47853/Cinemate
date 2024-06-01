629ddabc83e3e1772c462f5c457a015a915b759a
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-06-01 10:47:52 +0200

    Add Role enum

--- src/main/java/pl/patrykjava/cinemate/member/Role.java
+++ src/main/java/pl/patrykjava/cinemate/member/Role.java
@@ -1,18 +1,6 @@
 package pl.patrykjava.cinemate.member;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Entity
-@Table(name = "role")
-public class Role {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
 }
